5f6de5beef87e6be208a5e9e1e6e1936a6f28dcc
Ludovico Spitaleri <ludovico.spitaleri@studio.unibo.it>
2024-12-27 11:12:26 +0100

    game object position

--- src/main/java/arcaym/model/game/core/objects/api/GameObject.java
+++ src/main/java/arcaym/model/game/core/objects/api/GameObject.java
@@ -1,11 +1,26 @@
 package arcaym.model.game.core.objects.api;
 
+import arcaym.common.point.Point2D;
 import arcaym.model.game.core.world.api.GameWorld;
 
 /**
  * Interface for a basic game object.
  */
 public interface GameObject {
+
+    /**
+     * Set object position.
+     * 
+     * @param position new position
+     */
+    void setPosition(Point2D position);
+
+    /**
+     * Get object position.
+     * 
+     * @return position
+     */
+    Point2D getPosition();
 
     /**
      * Setup object for world.
