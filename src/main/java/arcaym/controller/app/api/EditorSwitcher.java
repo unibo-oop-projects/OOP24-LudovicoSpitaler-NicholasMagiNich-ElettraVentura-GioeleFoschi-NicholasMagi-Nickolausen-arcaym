f2f5f7e640794d48cb2a2eaf04a6be64c08eb848
Gioele Foschi <gioele.foschi@studio.unibo.it>
2025-01-13 18:14:37 +0100

    removed old editor interface

--- src/main/java/arcaym/controller/app/api/EditorSwitcher.java
+++ src/main/java/arcaym/controller/app/api/EditorSwitcher.java
@@ -1,5 +1,7 @@
 package arcaym.controller.app.api;
 
+import arcaym.controller.editor.api.Editor;
+
 /**
  * An interface exposing the method used to switch from the playable level to the editor.
  */
@@ -9,5 +11,5 @@ public interface EditorSwitcher {
      * Switches context from the level to the editor.
      * @return the controller used by the editor.
      */
-    EditorController returnToEditor();
+    Editor returnToEditor();
 }
