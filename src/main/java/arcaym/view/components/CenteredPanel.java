01d0e33dcf87bf91dc0be6673dcd52b49dc152d1
Ludovico Spitaleri <ludovico.spitaleri@studio.unibo.it>
2025-01-24 22:36:06 +0100

    utility view classes

---
+++ src/main/java/arcaym/view/components/CenteredPanel.java
@@ -0,0 +1,43 @@
+package arcaym.view.components;
+
+import java.util.Objects;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import arcaym.view.api.GeneralSwingView;
+import arcaym.view.app.levels.LevelsList;
+import arcaym.view.utils.SwingUtils;
+
+public class CenteredPanel implements GeneralSwingView<JPanel> {
+
+    private final JComponent content;
+
+    public CenteredPanel(final JComponent content) {
+        this.content = Objects.requireNonNull(content);
+    }
+
+    public CenteredPanel(final GeneralSwingView<?> content) {
+        this(content.build());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JPanel build() {
+        final var panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        panel.add(Box.createHorizontalGlue());
+        panel.add(this.content);
+        panel.add(Box.createHorizontalGlue());
+        return panel;
+    }
+
+    public static void main(String[] args) {
+        SwingUtils.testComponent(new CenteredPanel(new LevelsList()));
+    }
+
+}
---
+++ src/main/java/arcaym/view/utils/SwingUtils.java
@@ -0,0 +1,104 @@
+package arcaym.view.utils;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import arcaym.view.api.GeneralSwingView;
+
+/**
+ * Utility class for swing.
+ */
+public final class SwingUtils {
+
+    /**
+     * Default screen size.
+     */
+    public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+
+    /**
+     * {@link Color} that represents transparency.
+     */
+    public static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
+
+    private static final float NORMAL_GAP_FACTOR = 1f;
+    private static final float LITTLE_GAP_FACTOR = 0.5f;
+    private static final float BIG_GAP_FACTOR = 2f;
+
+    private SwingUtils() { }
+
+    /**
+     * Change font size of a component.
+     * 
+     * @param component swing component
+     * @param scaleFactor font scale factor
+     */
+    public static void changeFontSize(final JComponent component, final float scaleFactor) {
+        final var font = component.getFont();
+        component.setFont(font.deriveFont(font.getSize() * scaleFactor));
+    }
+
+    /**
+     * Get gap value based on the font size of the component.
+     * 
+     * @param component swing component
+     * @param scaleFactor gap scale factor
+     */
+    public static int getGap(final JComponent component, final float scaleFactor) {
+        return Math.round(component.getFont().getSize() * scaleFactor);
+    }
+
+    /**
+     * Call {@link #getGap(JComponent, float)} with a normal size factor.
+     * 
+     * @param component swing component
+     */
+    public static int getNormalGap(final JComponent component) {
+        return getGap(component, NORMAL_GAP_FACTOR);
+    }
+
+    /**
+     * Call {@link #getGap(JComponent, float)} with a little size factor.
+     * 
+     * @param component swing component
+     */
+    public static int getLittleGap(final JComponent component) {
+        return getGap(component, LITTLE_GAP_FACTOR);
+    }
+
+    /**
+     * Call {@link #getGap(JComponent, float)} with a big size factor.
+     * 
+     * @param component swing component
+     */
+    public static int getBigGap(final JComponent component) {
+        return getGap(component, BIG_GAP_FACTOR);
+    }
+
+    /**
+     * Show a component in a test window.
+     * NEEDS TO BE REMOVED.
+     * 
+     * @param component component to show
+     */
+    public static void testComponent(final GeneralSwingView<?> component) {
+        // TODO remove
+        final var gap = 100;
+        final var frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final var panel = new JPanel();
+        frame.setContentPane(panel);
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(gap, gap, gap, gap));
+        panel.add(component.build(), BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
