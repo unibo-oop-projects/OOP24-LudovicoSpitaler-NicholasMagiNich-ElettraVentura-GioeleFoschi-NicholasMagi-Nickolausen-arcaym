90c218bac8a65d2459e6480eba1e64816b512a37
Ludovico Spitaleri <ludovico.spitaleri@studio.unibo.it>
2025-01-29 17:05:34 +0100

    levels list inside level panel

--- src/main/java/arcaym/view/app/menu/levels/LevelsPanel.java
+++ src/main/java/arcaym/view/app/menu/levels/LevelsPanel.java
@@ -1,15 +1,27 @@
 package arcaym.view.app.menu.levels;
 
-import javax.swing.JPanel;
+import java.util.List;
+import java.util.stream.Stream;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import arcaym.controller.editor.saves.LevelMetadata;
+import arcaym.controller.editor.saves.MetadataManagerImpl;
 import arcaym.view.app.panels.SwitchablePanel;
 import arcaym.view.app.panels.Switcher;
 import arcaym.view.components.CenteredPanel;
+import arcaym.view.utils.SwingUtils;
 
 /**
  * View for a levels panel.
  */
 public class LevelsPanel extends SwitchablePanel {
+
+    private final List<LevelMetadata> levels = new MetadataManagerImpl().loadData();
 
     /**
      * Initialize with given switcher.
@@ -25,7 +37,18 @@ public class LevelsPanel extends SwitchablePanel {
      */
     @Override
     public JPanel build() {
-        return new CenteredPanel().build(new LevelsList());
+        final var mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        final var gap = SwingUtils.getNormalGap(mainPanel);
+        levels.stream()
+            .sorted((l1, l2) -> l1.levelName().compareTo(l2.levelName()))
+            .map(LevelCard::new)
+            .map(LevelCard::build)
+            .flatMap(c -> Stream.of(Box.createVerticalStrut(gap), c))
+            .skip(1)
+            .forEach(mainPanel::add);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(gap, gap, gap, gap));
+        return new CenteredPanel().build(new JScrollPane(mainPanel));
     }
 
 }
