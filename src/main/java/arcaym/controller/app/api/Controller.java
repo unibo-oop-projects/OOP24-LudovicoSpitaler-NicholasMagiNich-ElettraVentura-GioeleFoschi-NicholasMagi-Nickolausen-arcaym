5b2ec1a068155998d17f8a433e4ab548cde93814
Gioele Foschi <gioele.foschi@studio.unibo.it>
2025-01-30 17:23:54 +0100

    added controller methods

--- src/main/java/arcaym/controller/app/api/Controller.java
+++ src/main/java/arcaym/controller/app/api/Controller.java
@@ -5,5 +5,21 @@ package arcaym.controller.app.api;
  * to correctly switch from each page.
  */
 public interface Controller {
+    /**
+     * Sets the necessary methods / objects.
+     */
+    void setView(/* Potentially generic stuff */);
 
+    /**
+     * Executes the given function on the return action.
+     * Only temporary, will possibly be affected by future changes
+     * 
+     * @param onReturn The function to run
+     */
+    void onReturn(Runnable onReturn);
+
+    /**
+     * This function will be called if the application is closed.
+     */
+    void runOnClose();
 }
