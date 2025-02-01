e653bfa0dde93f52acc5a0210be49e66710bb631
Elettra Ventura <elettra.ventura@studio.unibo.it>
2025-02-01 18:22:59 +0000

    introducint view of already owned objects (wip)

--- src/main/java/arcaym/view/shop/ShopView.java
+++ src/main/java/arcaym/view/shop/ShopView.java
@@ -5,7 +5,6 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -17,8 +16,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import arcaym.controller.shop.api.ShopController;
 import arcaym.model.game.core.objects.api.GameObjectCategory;
-import arcaym.model.game.objects.api.GameObjectType;
 import arcaym.view.components.CenteredPanel;
 import arcaym.view.core.api.ViewComponent;
 import arcaym.view.core.api.WindowInfo;
@@ -32,61 +31,45 @@ public class ShopView implements ViewComponent<JPanel> {
     private static final String SHOP_TITLE = "SHOP";
     private static final String BUY_BUTTON = "PURCHASE";
     private static final String BACK_TO_MAIN_PAGE = "BACK TO MENU";
-    // private final ShopController controller = new ShopControllerImpl();
+    private final ShopController controller = null;
     private final Map<JButton, ProductInfo> productsMap = new HashMap<>();
     private Optional<ProductInfo> selected = Optional.empty();
     private JButton buyButton;
-    private int counterTHISISAMOCKTODELETE = 10;
-
-    private final Map<GameObjectType, Integer> mockController = new EnumMap<>(GameObjectType.class); // mock
 
     /**
      * {@inheritDoc}
      */
     @Override
     public JPanel build(final WindowInfo window) {
-        mockController.put(GameObjectType.COIN, SCALE);
-        mockController.put(GameObjectType.MOVING_X_OBSTACLE, SCALE);
-        mockController.put(GameObjectType.MOVING_Y_OBSTACLE, SCALE);
-        mockController.put(GameObjectType.FLOOR, SCALE); //
-
         final JPanel contentPanel = new JPanel();
         final var gap = SwingUtils.getBigGap(contentPanel);
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
-
         final JLabel title = new JLabel(SHOP_TITLE);
         SwingUtils.changeFontSize(title, SCALE);
         contentPanel.add(new CenteredPanel().build(window, title));
-
-        final JLabel userCredit = new JLabel("Credit: " + String.valueOf(counterTHISISAMOCKTODELETE));
+        final JLabel userCredit = new JLabel();
+        updateCreditLable(userCredit);
         contentPanel.add(new CenteredPanel().build(window, userCredit));
-
         final JScrollPane scrollPanel = new JScrollPane();
         final JPanel itemsPanel = new JPanel();
         itemsPanel.setMinimumSize(scrollPanel.getSize());
         itemsPanel.setLayout(new GridLayout(0, 1));
         scrollPanel.setViewportView(itemsPanel);
         fillItems(window, itemsPanel);
-
         contentPanel.add(Box.createVerticalStrut(gap));
-
         scrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         contentPanel.add(scrollPanel);
-
         contentPanel.add(Box.createVerticalStrut(gap));
-
         this.buyButton = new JButton(BUY_BUTTON);
         buyButton.setEnabled(false);
-
         final ActionListener purchaseAl = new ActionListener() {
 
             @Override
             public void actionPerformed(final ActionEvent arg) {
                 if (selected.isPresent()) {
                     // controllerBUY();
-                    counterTHISISAMOCKTODELETE--;
-                    userCredit.setText("Credit: " + String.valueOf(counterTHISISAMOCKTODELETE));
+                    updateCreditLable(userCredit);
                     //
                     buyButton.setEnabled(false);
                     selected = Optional.empty();
@@ -110,6 +93,10 @@ public class ShopView implements ViewComponent<JPanel> {
         return contentPanel;
     }
 
+    private void updateCreditLable(final JLabel userCredit) {
+        userCredit.setText("Credit : " + controller.getCredit());
+    }
+
     private void fillItems(final WindowInfo window, final JPanel itemsPanel) {
         for (final var category : GameObjectCategory.values()) {
             final JPanel categoryPanel = createCategoryCard(window, category);
@@ -131,29 +118,17 @@ public class ShopView implements ViewComponent<JPanel> {
         final JPanel showItemsPanel = new JPanel();
         showItemsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        // final int maxItems = (int)
-        // controller.getLockedGameObjects().keySet().stream()
-        // .filter(type -> type.category() == category).count();
-        // if (maxItems == 0) {
-        // showItemsPanel.setBackground(Color.GRAY);
-        // showItemsPanel.add(new JLabel("No Items Available."));
-        // } else {
-        // for (var object : controller.getLockedGameObjects().entrySet()) {
-        // JPanel item = new DisplayProductView(object.getKey(),
-        // object.getValue()).build(window);
-        // item.setPreferredSize(SwingUtils.scaleDimension(item.getPreferredSize(),
-        // SCALE));
-        // showItemsPanel.add(item);
-        // }
-        // }
-
-        final int maxItems = (int) mockController.keySet().stream()
-                .filter(type -> type.category().equals(category)).count();
-        if (maxItems == 0) {
+        final int maxAvailableItems = (int) controller.getLockedGameObjects().keySet().stream()
+                .filter(type -> type.category() == category).count();
+        // final int maxAlreadyOwnedItems = (int)
+        // controller.????.keySet().stream().filter(type->type.category() ==
+        // category).count();
+        final int maxItems = maxAvailableItems; // Math.max(maxAvailableItems, maxAlreadyOwnedItems);
+        if (maxItems == 0) { // if (maxAvailableItems == 0 && maxAlreadyOwnedItems == 0)
             showItemsPanel.setBackground(Color.GRAY);
             showItemsPanel.add(new JLabel("No Items Available."));
-        } else {
-            for (final var object : mockController.entrySet().stream()
+        } else { // else { if(maxAvailableItems != 0)
+            for (final var object : controller.getLockedGameObjects().entrySet().stream()
                     .filter(type -> type.getKey().category().equals(category)).toList()) {
                 final JPanel item = new DisplayProductView(new ProductInfo(object.getKey(), object.getValue()))
                         .build(window);
@@ -181,8 +156,8 @@ public class ShopView implements ViewComponent<JPanel> {
                 item.add(new CenteredPanel().build(window, price));
                 showItemsPanel.add(item);
             }
-        }
-
+        } // if(maxAlreadyOwnedItems != 0)      }
+        // same thing as before but with the map of already owned objects.
         card.add(showItemsPanel);
         return card;
     }
@@ -197,11 +172,11 @@ public class ShopView implements ViewComponent<JPanel> {
 
     private void setAvailableButtons() {
         productsMap.entrySet().forEach(product -> {
-            product.getKey().setEnabled(true);
-            product.getKey().setBackground(Color.WHITE);
+            if (controller.canBuy(product.getValue().type())) {
+                product.getKey().setEnabled(true);
+                product.getKey().setBackground(Color.WHITE);
+            }
         });
-        // .setEnabled(controller.requestTransaction(product.getValue().type()) ? true :
-        // false));
     }
 
     /**
