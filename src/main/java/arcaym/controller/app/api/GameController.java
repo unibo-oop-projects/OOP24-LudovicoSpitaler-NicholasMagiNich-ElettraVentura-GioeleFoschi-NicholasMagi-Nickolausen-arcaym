8d507984688574981aa75ede1e1f4d27076a735f
Elettra Ventura <elettra.ventura@studio.unibo.it>
2025-02-03 13:02:50 +0000

    minor changes

--- src/main/java/arcaym/controller/app/api/GameController.java
+++ src/main/java/arcaym/controller/app/api/GameController.java
@@ -1,8 +1,16 @@
 package arcaym.controller.app.api;
 
+import arcaym.model.game.core.engine.api.Game;
+
 /**
  * The controller for the playable level.
  */
 public interface GameController {
+
+    void startGame();
+
+    Game getGame();
+
+    
 
 }
