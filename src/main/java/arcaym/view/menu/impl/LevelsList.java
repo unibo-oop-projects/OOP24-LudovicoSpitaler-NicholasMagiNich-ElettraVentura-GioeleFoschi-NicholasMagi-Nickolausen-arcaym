2232a4da9e5ea0363bf48128ad77fec992ee3025
Ludovico Spitaleri <ludovico.spitaleri@studio.unibo.it>
2025-02-04 09:42:23 +0100

    invisible levels list when empty

--- src/main/java/arcaym/view/menu/impl/LevelsList.java
+++ src/main/java/arcaym/view/menu/impl/LevelsList.java
@@ -42,14 +42,16 @@ public class LevelsList implements ViewComponent<JScrollPane> {
         final var mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
         final var gap = SwingUtils.getNormalGap(mainPanel);
-        levels.stream()
+        this.levels.stream()
             .sorted((l1, l2) -> l1.levelName().compareTo(l2.levelName()))
             .map(metadata -> new LevelCard(metadata, this.levelOpener).build(window))
             .flatMap(c -> Stream.of(Box.createVerticalStrut(gap), c))
             .skip(1)
             .forEach(mainPanel::add);
         mainPanel.setBorder(BorderFactory.createEmptyBorder(gap, gap, gap, gap));
-        return new JScrollPane(mainPanel);
+        final var scrollPanel = new JScrollPane(mainPanel);
+        scrollPanel.setVisible(!this.levels.isEmpty());
+        return scrollPanel;
     }
 
 }
